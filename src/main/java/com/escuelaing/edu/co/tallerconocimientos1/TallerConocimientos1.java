e169a9bcb79e4433310789444a39d64bc5526fb9
unknown <ivan.forero-t@labinfo.is.escuelaing.edu.co>
2025-09-09 09:02:28 -0500

    Scarffolding

---
+++ src/main/java/com/escuelaing/edu/co/tallerconocimientos1/TallerConocimientos1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.escuelaing.edu.co.tallerconocimientos1;
+
+/**
+ *
+ * @author ivan.forero-t
+ */
+public class TallerConocimientos1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
